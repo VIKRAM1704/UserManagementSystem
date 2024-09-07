309d9120b00112ccb745b9c350ad056d8052003e
Vikram V <106013403+VIKRAM1704@users.noreply.github.com>
2024-09-07 23:27:40 +0530

    Delete HomeJavaScript.js

--- HomeJavaScript.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const menuToggle = document.querySelector(".menu-toggle");
-  const navUl = document.querySelector("nav ul");
-
-  menuToggle.addEventListener("click", function () {
-    navUl.classList.toggle("active");
-  });
-});
